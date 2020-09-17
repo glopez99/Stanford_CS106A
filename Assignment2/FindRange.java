90092bcf559d3e448b0e0fc3e58b10bfc53b0458
Greg Lopez <42146599+glopez0909@users.noreply.github.com>
2020-09-17 15:05:23 -0400

    Solved the final two problems
    solved Find Range and Hailstone Sequence

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -10,7 +10,39 @@ import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
-	}
+		println("This program finds the largest and smallest numbers.");
+
+		//sets up the terms for the sentinel, largest, & smallest values
+		int SENTINEL = 0;
+		int value  = readInt("?");
+		int smallestValue = value;
+		int largestValue = value;
+
+		//checks if sentinel was entered as the first number
+		if(value == SENTINEL) {
+			println("No valid values have been entered.");
+			return;
+
+		};
+
+		//continues to have the user enter numbers as long as the sentinel
+		//value hasn't been entered. This also keeps track of the largest
+		//and lowest values.
+		while (value != SENTINEL) {
+
+			if (value >= largestValue) {
+				largestValue = value;
+			} else if (value <= smallestValue) {
+				smallestValue = value;
+			}
+
+			value = readInt("?");
+		};
+
+		//returns the output for the largest and smallest number
+		println("Smallest value: " + smallestValue);
+		println("Largest value: " + largestValue);
+
+	};
 }
 
